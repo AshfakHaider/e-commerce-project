5d7603d86780b0c799b502b0713aa0ab159a31d5
nAzmulHudaa <nhr.developer@gmail.com>
2021-06-28 19:44:49 +0600

    product list updated

--- src/Components/Product/ProductDetails.js
+++ src/Components/Product/ProductDetails.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import './ProductDetails.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
-const ProductDetails = ({product}) => {
-    const {name,image,quantity,price} = product;
+const ProductDetails = ({ product }) => {
+    const { name, image, quantity, price } = product;
     return (
-        <div className="col-md-3 justify-content-center pdCard">
-            <div className="pdImg">
-                <img src={image} alt="" className='w-100' />
-            </div>
-            <div className="pd-info">
-                <h6>{name}</h6>
-                <span>{quantity} pcs</span>
-                <div className="d-flex justify-content-lg-between pd-price">
-                    <div className="price">
-                        <h6>{price}</h6>
-                    </div>
-                    <div className="cart">
-                        <h6>Add To Cart</h6>
+        <div className='col-md-3 justify-content-center'>
+            <div className=" pdCard">
+                <div className="pdImg">
+                    <img src={image} alt="" className='w-100' />
+                </div>
+                <div className="pd-info">
+                    <h6>{name}</h6>
+                    <span>{quantity} pcs</span>
+                    <div className="d-flex justify-content-lg-between pd-price">
+                        <div className="price">
+                            <h6>{price}</h6>
+                        </div>
+                        <div className="cart d-flex">
+                           
+                            <button className='cartBtn'> <FontAwesomeIcon icon={faShoppingCart} className='cartIcon' /> Cart</button>
+                        </div>
                     </div>
                 </div>
             </div>
+
         </div>
     );
 };
