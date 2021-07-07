f6b058896d32acdbcc0e2e24eef9741de0dcce39
nAzmulHudaa <your_email@example.comasdasasdasdasaadasdsadaasd>
2021-07-07 21:01:01 +0600

    sidebar fixed

--- src/Components/SidebarTest/SidebarTest.js
+++ src/Components/SidebarTest/SidebarTest.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import * as FaIcons from 'react-icons/fa';
@@ -7,6 +7,8 @@ import { SidebarData } from '../SidebarTest/SidebarData';
 import SubMenu from './Submenu';
 import { IconContext } from 'react-icons/lib';
 import './SidebarDash.css'
+import { useMediaQuery } from 'react-responsive'
+
 
 const Nav = styled.div`
   display: flex;
@@ -44,20 +46,54 @@ const SidebarWrap = styled.div`
 
 const SidebarTest = () => {
   const [sidebar, setSidebar] = useState(true);
+  const handleResize = () => {
+    if (window.innerWidth < 720) {
+      setSidebar(false)
+    } else {
+      setSidebar(true)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize)
+  })
+
+  const showSidebar = () => setSidebar(!sidebar);
+
+  const isDesktopOrLaptop = useMediaQuery({
+    query: '(min-device-width: 1224px)'
+  })
+  const isTabletOrMobile = useMediaQuery({ query: ' (min-width: 481px) and (max-width: 767px) ' })
+  const isTabletOrMobileDevice = useMediaQuery({
+    query: '(min-width: 320px) and (max-width: 480px) '
+  })
+
   
+  //choose the screen size 
+  
+
+
 
   return (
     <>
       <IconContext.Provider value={{ color: '#009E7F' }}>
-        <h3>Dashboard</h3>
+        <Nav>
+          <NavIcon to='#'>
+            <FaIcons.FaBars onClick={showSidebar} />
+          </NavIcon>
+        </Nav>
         <div className="sidebarWrap">
-        <SidebarNav sidebar={sidebar}> 
-          <SidebarWrap>
-            {SidebarData.map((item, index) => {
-              return <SubMenu item={item} key={index} />;
-            })}
-          </SidebarWrap>
-        </SidebarNav>
+          <SidebarNav sidebar={sidebar}>
+            <SidebarWrap>
+              <NavIcon to='#'>
+                <AiIcons.AiOutlineClose onClick={showSidebar} />
+              </NavIcon>
+
+              {SidebarData.map((item, index) => {
+                return <SubMenu item={item} key={index} />;
+              })}
+            </SidebarWrap>
+          </SidebarNav>
         </div>
       </IconContext.Provider>
     </>
